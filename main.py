7418a80e3f294f5c84eaa62cae566a77e8bd2799
fintech21-web <fintechinvestmentregister@gmail.com>
2025-11-29 21:02:19 +0300

    main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Messa
 TOKEN = os.getenv("BOT_TOKEN")
 
 # Telegram file_id for the receipt image
-FILE_ID = "AgACAgQAAxkBAAMbaSWTUbtSyaaO4nk7uY39DDptUOAAAsMLaxvFeSlRKhnYtBpPTJMBAAMCAAN5AAM2BA"
-FILE_ID2 = "AgACAgQAAxkBAANcaSrwHpp87IGuR-E-X5_maryIYfMAAgILaxtiBlhRe2GwQsDSxZoBAAMCAAN5AAM2BA"
-FILE_ID3 = "AgACAgQAAxkBAANSaSruVVLPa9HZ4xpOuKxQ812cYsgAAlsMaxtiBlBRJ5bdl0STQcEBAAMCAAN5AAM2BA"
+FILE_ID = "AgACAgQAAxkBAANcaSrwHpp87IGuR-E-X5_maryIYfMAAgILaxtiBlhRe2GwQsDSxZoBAAMCAAN5AAM2BA"
+FILE_ID2 = "AgACAgQAAxkBAANSaSruVVLPa9HZ4xpOuKxQ812cYsgAAlsMaxtiBlBRJ5bdl0STQcEBAAMCAAN5AAM2BA"
+FILE_ID3 = "AgACAgQAAxkBAANnaSsujjN39mpQCIy6Db3UbB1GvqwAAm4LaxtiBlhR5vDonYQVez0BAAMCAAN5AAM2BA"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "የካናዳ ፕሮሰስ በ ስራ እና ክህሎት ሚንስቴር በኩል ለመጀመር በቀጣይ የሚያገኙትን ፎርሞች ሞልተው የመመዝገቢያ 3,420 ብር ይክፈሉ። "
-        "ለመክፈል ይህን ይጫኑ /pay."
+        "የካናዳ ፕሮሰስ በ ስራ እና ክህሎት ሚኒስቴር በኩል ለመጀመር በቀጣይ የሚያገኙትን ፎርሞች ሞልተው የመመዝገቢያ 3,420 ብር ይክፈሉ። "
+        "ፎርሙን ለመሙላት እና የመመዝገቢያ ክፍያውን ለመክፈል ይህን ይጫኑ /pay."
     )
 
 
@@ -23,19 +23,19 @@ async def pay(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Send image using Telegram file_id
     await update.message.reply_photo(
         photo=FILE_ID,
-        caption=" *የተመደበሎት አምባሳደር ወይም ህጋዊ ወኪል:*",
+        caption=" *የ ኢትዮጵያ ፌዴራላዊ ዲሞክራሲያዊ ሪፐብሊክ የውጭ ጉዳይ ሚኒስቴር የምዝገባ ቅፅ።::*",
         parse_mode="Markdown"
     )
 # Send second image using Telegram file_id 
     await update.message.reply_photo( 
         photo=FILE_ID2, 
-        caption=" *የ ኢትዮጵያ ፌዴራላዊ ዲሞክራሲያዊ ሪፐብሊክ የውጭ ጉዳይ ሚኒስቴር የምዝገባ ቅፅ።:*", 
+        caption=" *የ ስራና ክህሎት የ ምዝገባ እና የ ስራ ዝርዝር ቅፅ።:*", 
         parse_mode="Markdown" )
 
     # Send third image using Telegram file_id 
     await update.message.reply_photo( 
         photo=FILE_ID3, 
-        caption=" *የ ስራና ክህሎት የ ምዝገባ እና የ ስራ ዝርዝር ቅፅ።:*", 
+        caption=" *የተመደበሎት አምባሳደር ወይም ህጋዊ ወኪል:*", 
         parse_mode="Markdown" )
     
     # Then send text instructions
@@ -43,9 +43,12 @@ async def pay(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "💰 የመክፈያ መመሪያ:\n\n"
         "ክፍያዎን ከታች በተቀመጠው የባንክ አካውንት ይላኩ:\n\n"
         "🏦 የአካውንት ስም: ሸጋው ታምሩ ተመስገን\n"
-        "💳 የአካውንት: 567592816011\n"
+        "💳 የአካውንት ቁጥር: 567592816011\n"
         "🏦 የባንክ ስም: ዳሽን ባንክ\n\n"
-        "ክፍያዎን ከከፈሉ በኋላ የክፍያ ደረሰኙን በ @bkuelmis ይላኩ።"
+        "የሞሉትን ፎርም እና ክፍያ የከፈሉበትን ደረሰኝ በ @ELMISofficial የቴሌ-ግራም አካውንት ይላኩ።"
+        "ሙሉ ማስረጃዎትን በላኩ ከ 2 እስከ 6 ሰአት ውስጥ ማሕተም የተረገጠበትን ፎርም እና የክፋያ ማረጋገጫ ደረሰኝ ይላክሎታል።"
+        "ለበለጠ መረጃ ጉዳዮን ለ ያዘዉ ኢምባሲ፣ ኤጀንሲ፣ ወይም ተወካይ ይደውሉ።"
+        
     )
     await update.message.reply_text(message, parse_mode="Markdown")
 
